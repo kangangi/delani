d6ec6079db3c087e02ab11343a4340a28271a7e3
kangangi <wanjikukangangi@gmail.com>
2020-02-23 19:32:37 +0300

    Add hovering effect to images

--- js/scripts.js
+++ js/scripts.js
@@ -28,4 +28,102 @@ $(document).ready(function(){
     $("#product p").hide();
     $("#product img").show();
   });
+
+//hovering function
+$('#work1 img').hover(function () {
+    $(this).stop().animate({
+    opacity: .4
+    });
+    $('.caption1').removeClass('hide');
+    }, function () {
+    $(this).stop().animate({
+      opacity: 1
+    });
+    $('.caption1').addClass('hide');
+    });
+
+  $('#work2 img').hover(function () {
+      $(this).stop().animate({
+        opacity: .4
+        });
+      $('.caption2').removeClass('hide');
+        }, function () {
+      $(this).stop().animate({
+          opacity: 1
+        });
+      $('.caption2').addClass('hide');
+        });
+
+    $('#work3 img').hover(function () {
+      $(this).stop().animate({
+              opacity: .4
+              });
+      $('.caption3').removeClass('hide');
+              }, function () {
+      $(this).stop().animate({
+                opacity: 1
+              });
+      $('.caption3').addClass('hide');
+              });
+
+  $('#work4 img').hover(function () {
+    $(this).stop().animate({
+        opacity: .4
+      });
+    $('.caption4').removeClass('hide');
+      }, function () {
+    $(this).stop().animate({
+      opacity: 1
+    });
+  $('.caption4').addClass('hide');
+  });
+
+  $('#work5 img').hover(function () {
+    $(this).stop().animate({
+        opacity: .4
+      });
+    $('.caption5').removeClass('hide');
+      }, function () {
+    $(this).stop().animate({
+      opacity: 1
+    });
+  $('.caption5').addClass('hide');
+  });
+
+  $('#work6 img').hover(function () {
+    $(this).stop().animate({
+        opacity: .4
+      });
+    $('.caption6').removeClass('hide');
+      }, function () {
+    $(this).stop().animate({
+      opacity: 1
+    });
+  $('.caption6').addClass('hide');
+  });
+
+  $('#work7 img').hover(function () {
+    $(this).stop().animate({
+        opacity: .4
+      });
+    $('.caption7').removeClass('hide');
+      }, function () {
+    $(this).stop().animate({
+      opacity: 1
+    });
+  $('.caption7').addClass('hide');
+  });
+
+  $('#work8 img').hover(function () {
+    $(this).stop().animate({
+        opacity: .4
+      });
+    $('.caption8').removeClass('hide');
+      }, function () {
+    $(this).stop().animate({
+      opacity: 1
+    });
+  $('.caption8').addClass('hide');
+  });
+
 });
